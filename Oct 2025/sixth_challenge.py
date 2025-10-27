3ed5ccc63058e094280621144806350aef6f19b9
ingdev8023 <ingdev8023@gmail.com>
2025-10-26 23:30:21 -0500

    starting to better understand how to work with lists and dics in this challenge

--- Oct 2025/sixth_challenge.py
+++ Oct 2025/sixth_challenge.py
@@ -1,28 +1,28 @@
-#learning objects and class in python
-class Vehicle:
-    color = "white"
+# Favorite Songs
+# Remember iPods? The first model came out 24 years ago today, on Oct. 23, 2001.
 
+# Given an array of song objects representing your iPod playlist, return an array with the titles of the two most played songs, with the most played song first.
 
-    def __init__(self, max_speed, mileage):
-        self.max_speed = max_speed
-        self.mileage = mileage
-        self.seating_capacity = None
+# Each object will have a "title" property (string), and a "plays" property (integer).
 
+def favorite_songs(playlist):
+    plays = []
+    title = []
+    most_played_songs = []
+    for song in playlist:
+        plays.append(song['plays'])
+        title.append(song['title'])
+    
+    while len(most_played_songs) < 2:
+        index = plays.index(max(plays))
+        most_played_songs.append(title[index])
+        plays.pop(index)
+        title.pop(index)
+               
+    return most_played_songs
 
-    def assign_seating_capacity(self, seating_capacity):
-        self.seating_capacity = seating_capacity
+favorite_songs([{"title": "Sync or Swim", "plays": 3}, {"title": "Byte Me", "plays": 1}, {"title": "Earbud Blues", "plays": 2} ])
+favorite_songs([{"title": "Skip Track", "plays": 98}, {"title": "99 Downloads", "plays": 99}, {"title": "Clickwheel Love", "plays": 100} ])
+favorite_songs([{"title": "Song A", "plays": 42}, {"title": "Song B", "plays": 99}, {"title": "Song C", "plays": 75} ])
 
-
-V1 = Vehicle(150, 25)
-V1.color = 'red'
-print(V1.color)
-
-class Graph():
-    def __init__(self, id):
-        self.id = id
-        self.id = 80
-
-
-val = Graph(200)
-print(val.id)
 
