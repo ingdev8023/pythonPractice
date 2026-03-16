46d2a7edb8e913986ce11a7d89f4bcc3463e27bc
ingdev8023 <ingdev8023@gmail.com>
2026-03-16 13:53:31 -0500

    I built a small exercise using just my memory to check that I remember things correctly

---
+++ March 2026/some exercises/daily challenges/brain_check.py
@@ -0,0 +1,12 @@
+import sys
+import re
+
+log_file = sys.argv[1]
+
+with open(log_file) as f:
+    for line in f:
+        match_error = re.search(r"ticky: ERROR (.*)\((.*)\)", line)
+        if match_error:
+            message = match_error.group(1).strip()
+            user = match_error.group(2)
+            print(f"ERROR | {user} | {message}")
