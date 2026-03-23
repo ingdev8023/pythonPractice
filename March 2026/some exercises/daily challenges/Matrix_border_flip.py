105e749fed5cf566ffbc5c9aee410e727a2055c6
ingdev8023 <ingdev8023@gmail.com>
2026-03-23 12:59:54 -0500

    one extra harder matrix flip challenge

--- March 2026/some exercises/daily challenges/Matrix_border_flip.py
+++ March 2026/some exercises/daily challenges/Matrix_border_flip.py
@@ -10,5 +10,37 @@ last column
 All inner elements must stay the same. """
 
 def matrix_border_flip(matrix):
-    
-    return '' 
+    new_matrix = []
+    values = set()
+    for i in matrix:
+        for j in i:
+            values.add(j)
+    unique1,unique2 = values
+
+    for i in range(len(matrix)):
+        new_row = []
+        if i == 0 or i == len(matrix) - 1:
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == unique1:
+                    new_row.append(unique2)
+                else:
+                    new_row.append(unique1)             
+        else:
+            for j in range(len(matrix[i])):
+                if j == 0 or j == len(matrix[i]) - 1:
+                    if matrix[i][j] == unique1:
+                        new_row.append(unique2)
+                    else:
+                        new_row.append(unique1)  
+                else:
+                    new_row.append(matrix[i][j])
+
+        new_matrix.append(new_row)    
+    return new_matrix 
+
+print(matrix_border_flip([
+    [1, 0, 1, 1],
+    [0, 1, 0, 1],
+    [1, 1, 0, 0],
+    [0, 0, 1, 1]
+]))
