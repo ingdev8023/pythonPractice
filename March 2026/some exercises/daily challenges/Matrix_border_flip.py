967410683d4bef64bfdbf1bd6fad2f7767c1c3c1
ingdev8023 <ingdev8023@gmail.com>
2026-03-23 13:01:52 -0500

    another approach for the matrix flip

--- March 2026/some exercises/daily challenges/Matrix_border_flip.py
+++ March 2026/some exercises/daily challenges/Matrix_border_flip.py
@@ -44,3 +44,39 @@ print(matrix_border_flip([
     [1, 1, 0, 0],
     [0, 0, 1, 1]
 ]))
+
+
+
+#chat approach
+
+def matrix_border_flip(matrix):
+    new_matrix = []
+    values = set()
+
+    for row in matrix:
+        for val in row:
+            values.add(val)
+
+    unique1, unique2 = values
+
+    for i in range(len(matrix)):
+        new_row = []
+        for j in range(len(matrix[i])):
+            is_border = (
+                i == 0 or
+                i == len(matrix) - 1 or
+                j == 0 or
+                j == len(matrix[i]) - 1
+            )
+
+            if is_border:
+                if matrix[i][j] == unique1:
+                    new_row.append(unique2)
+                else:
+                    new_row.append(unique1)
+            else:
+                new_row.append(matrix[i][j])
+
+        new_matrix.append(new_row)
+
+    return new_matrix
