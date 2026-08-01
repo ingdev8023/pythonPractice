1e04b786c35fd4b5773425ba3c051fb66a8cf54f
ingdev8023 <ingdev8023@gmail.com>
2026-08-01 12:39:15 -0500

    Practice:  class and object freecode camp,  lab freecodecamp

---
+++ July 2026/class_objects_practices1.py
@@ -0,0 +1,27 @@
+class Planet:
+    def __init__(self, name, planet_type, star):
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+    
+        if type(name)  != str or type(planet_type)  !=  str or   type(star) != str:
+            raise TypeError('name, planet type, and star must be strings')
+        if not name or not planet_type or not star:
+            raise ValueError('name, planet_type, and star must be non-empty strings')
+    def orbit(self):
+        return f'{self.name} is orbiting around {self.star}...'
+    def __str__(self):
+        return f'Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}'
+
+planet_1 = Planet('earth', 'test', 'sun')
+planet_2 = Planet('mars', 'test2', 'sun')
+planet_3 = Planet('Jupiter', 'test3', 'sun')
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+
